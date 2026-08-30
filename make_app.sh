7e0995f035f36a7404a54e451bf283c0099466c5
emanon <emanon@emanondeMacBook-Air.local>
2026-08-30 19:02:10 +0800

    Release: 版本号 2.8（三处同步：app Info.plist / helper Info.plist / helperVersion）

--- make_app.sh
+++ make_app.sh
@@ -61,8 +61,8 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<PLIST
     <key>CFBundleName</key><string>$APP_NAME</string>
     <key>CFBundleDisplayName</key><string>DiskProbe</string>
     <key>CFBundleIdentifier</key><string>local.diskprobe</string>
-    <key>CFBundleVersion</key><string>2.7</string>
-    <key>CFBundleShortVersionString</key><string>2.7</string>
+    <key>CFBundleVersion</key><string>2.8</string>
+    <key>CFBundleShortVersionString</key><string>2.8</string>
     <key>CFBundlePackageType</key><string>APPL</string>
     <key>CFBundleExecutable</key><string>$APP_NAME</string>
     <key>LSMinimumSystemVersion</key><string>11.0</string>
